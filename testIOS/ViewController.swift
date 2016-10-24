1e89497bd183b5ec290c84692039aed97b0643e7
Abraham <grafica.mx@gmail.com>
2016-10-24 12:49:31 -0500

    Initial Commit

---
+++ testIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testIOS
+//
+//  Created by Abraham Tavera V. on 24/10/16.
+//  Copyright © 2016 MezcalDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
